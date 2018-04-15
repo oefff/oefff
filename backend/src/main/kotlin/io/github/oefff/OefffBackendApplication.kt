2f9c5195f4a8282598daa8276e87c15fb28974ac
Marco Beelen <marco.beelen@ximedes.com>
2018-04-15 09:20:41 +0200

    Enabled CORS on localhost:4200 for local development

--- backend/src/main/kotlin/io/github/oefff/OefffBackendApplication.kt
+++ backend/src/main/kotlin/io/github/oefff/OefffBackendApplication.kt
@@ -2,9 +2,27 @@ package io.github.oefff
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
 
 @SpringBootApplication
-class OefffBackendApplication
+class OefffBackendApplication {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+            }
+        }
+    }
+}
+
+
+
+
+
 
 fun main(args: Array<String>) {
     runApplication<OefffBackendApplication>(*args)
